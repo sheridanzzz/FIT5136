d53448c33a2136be4c218c5eb8153e693552af08
Xiaoyan Gu <xguu0009@student.monash.edu>
2019-10-20 20:42:21 +1100

    Final version of implementation

---
+++ pm_final/pm1.1/Hall.java
@@ -0,0 +1,212 @@
+import java.util.*;
+import java.time.LocalDate;
+/**
+ * Write a description of class Hall here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Hall
+{
+    private String hallName;
+    private HallOwner hallOwner;//deal with later
+    private String description;
+    private String address;
+    private float depositPercentage;
+    private float averageRating; //problem
+    private float hallBasePrice;
+    private int hallCapacity;
+    private HashMap<LocalDate,TimeSlot> datetimesNotAvailable;
+    private LocalDate updateDate;
+    private boolean hallIsAvailable;
+    private ArrayList<Review> reviews;
+
+    /**
+     * Constructor for objects of class Hall
+     */
+    public Hall()
+    {
+
+    }
+
+    public Hall(String newHallName, String newDesc,String newAddr,float newDepositPercen, float newHallBasePrice, int newHallCapacity,HashMap<LocalDate,TimeSlot> newDatetimesNotAvailable)
+    {
+        hallName = newHallName;
+        description = newDesc;
+        address = newAddr;
+        depositPercentage = newDepositPercen;
+        hallBasePrice = newHallBasePrice;
+        hallCapacity = newHallCapacity;
+        datetimesNotAvailable = newDatetimesNotAvailable;
+        hallIsAvailable = true;
+        reviews = new ArrayList<Review>();
+    }
+
+    public Hall(String newHallName, String newDesc,String newAddr,float newDepositPercen, float newHallBasePrice, int newHallCapacity)
+    {
+        hallName = newHallName;
+        description = newDesc;
+        address = newAddr;
+        depositPercentage = newDepositPercen;
+        hallBasePrice = newHallBasePrice;
+        hallCapacity = newHallCapacity;
+        hallIsAvailable = true;
+        datetimesNotAvailable = new HashMap<LocalDate,TimeSlot>();
+        reviews = new ArrayList<Review>();
+    }
+
+    public String getHallName()
+    {
+        return hallName;
+    }
+
+    public void setHallName(String newHallName)
+    {
+        hallName = newHallName;
+    }
+
+    public String getHallDesc()
+    {
+        return description;
+    }
+
+    public void setHallDesc(String newHallDesc)
+    {
+        description = newHallDesc;
+    }
+
+    public String getHallAddr()
+    {
+        return address;
+    }
+
+    public void setHallAddr(String newHallAddr)
+    {
+        address = newHallAddr;
+    }
+
+    public float getDeposPercen()
+    {
+        return depositPercentage;
+    }
+
+    public void setDeposPercen(float newDesposPrecen)
+    {
+        depositPercentage = newDesposPrecen;
+    }
+
+    public float getAverageRating()
+    {
+        return averageRating;
+    }
+
+    public void setAverageRating(float newAvgRating)
+    {
+        averageRating = newAvgRating;
+    }
+
+    public float getHallBasePrice()
+    {
+        return hallBasePrice;
+    }
+
+    public void setHallBasePrice(float newHallBasePrice)
+    {
+        hallBasePrice = newHallBasePrice;
+    }
+
+    public int getHallCapacity()
+    {
+        return hallCapacity;
+    }
+
+    public void setHallCapacity(int newCapacity)
+    {
+        hallCapacity = newCapacity;
+    }
+
+    public void setHallDateTime()
+    {
+
+    }
+
+    /**
+    public void addReview(Review review)
+    {
+    reviews.add(review);
+    float sum = 0;
+    for (Review areview: reviews)
+    {
+    sum += areview.getRatingNo();
+    }
+    averageRating = sum/reviews.size();
+
+        
+    }
+     **/
+
+    public boolean getHallIsAvailable()
+    {
+        return hallIsAvailable;
+    }
+
+    public void setHallIsAvailable(boolean newHallIsAvailable)
+    {
+        hallIsAvailable = newHallIsAvailable;
+    }
+
+    public LocalDate getUpdateDates()
+    {
+        return updateDate;
+    }
+
+    public void setUpdateDates(LocalDate newUpdateDates)
+    {
+        updateDate = newUpdateDates;
+    }
+
+    public HashMap<LocalDate,TimeSlot> getDatetimesNotAvailable()
+    {
+        return datetimesNotAvailable;
+    }
+
+    public void  setDatetimesNotAvailable(HashMap<LocalDate,TimeSlot> newDatetimesNotAvailable)
+    {
+        datetimesNotAvailable = newDatetimesNotAvailable; 
+    }
+
+    public ArrayList<Review> getReviews()
+    {
+        return reviews;
+    }
+
+    public void setBookings(ArrayList<Review> newReviews)
+    {
+        reviews = newReviews; 
+    }
+
+    public boolean checkDateAvailable(LocalDate date)
+    {   
+        boolean Isavail = false;
+        if(datetimesNotAvailable.containsKey(date))
+        {
+            //TimeSlot ts = datetimesNotAvailable.get(date);
+            if(datetimesNotAvailable.get(date).getAfternoon()||datetimesNotAvailable.get(date).getMorning()||datetimesNotAvailable.get(date).getEvening())
+            {
+                Isavail = true;
+            }
+
+        }
+        else 
+        {   
+            if (date.isAfter(LocalDate.of(2019, 10, 6)) && date.isBefore(LocalDate.of(2019, 10, 21)))
+            Isavail =  true;
+        }
+        return Isavail;
+    }
+
+    public void addReview()
+    {
+
+    }
+}
